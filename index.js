406850eb8fe3d11aa42a23b25d49538c8b233f94
Justin Kenel <jfkenel@gmail.com>
2017-02-22 21:41:04 -0500

    Server: Support setting port via environment variable

--- index.js
+++ index.js
@@ -97,8 +97,9 @@ function getApp(gameStates, gameEngines) {
     webSocket.send('test-connection');
   });
 
-  app.listen(3000, function () {
-    console.log('Running on port 3000!');
+  const port = process.env.PORT || 3000;
+  app.listen(port, function () {
+    console.log('Running on port: ' + port);
   });
 
   return app;
